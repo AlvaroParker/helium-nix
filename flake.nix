0cee795f5cb6c1cbe0c443c0ea91a9d576e19aab
AlvaroParker <64918109+AlvaroParker@users.noreply.github.com>
2025-12-17 10:28:07 +0000

    chore(update): helium to 0.7.6.1

--- flake.nix
+++ flake.nix
@@ -26,11 +26,11 @@
         helium = pkgs.appimageTools.wrapType2 rec {
 
           pname = "helium";
-          version = "0.7.5.1";
+          version = "0.7.6.1";
 
           src = pkgs.fetchurl {
             url = "https://github.com/imputnet/helium-linux/releases/download/${version}/${pname}-${version}-x86_64.AppImage";
-            sha256 = "sha256-Rn08KobbfMh3vlM2o0PA9OhQx/syMTBKWG/eapF45eo=";
+            sha256 = "sha256-SUpXcyQXUjZR57pNabVR/cSrGOMKvgzW0PSCLdB8d+E=";
           };
 
           extraInstallCommands =
